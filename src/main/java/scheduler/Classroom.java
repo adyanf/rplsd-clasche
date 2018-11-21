6f70057e54f0c862ff158080a9e14be0e98ec8df
Reiva5 <jehiannormansaviero@gmail.com>
2018-11-22 01:32:58 +0700

    Add Class Classroom

---
+++ src/main/java/scheduler/Classroom.java
@@ -0,0 +1,38 @@
+package scheduler;
+
+import java.util.Set;
+
+public class Classroom {
+    private String id;
+    private Integer capacity;
+    private Set<String> facilities;
+
+    public Classroom(String id, Integer capacity, Set<String> facilities) {
+        this.id = id;
+        this.capacity = capacity;
+        this.facilities = facilities;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public Set<String> getFacilities() {
+        return facilities;
+    }
+
+    public void removeFacilities(String facility) {
+        if (facilities.contains(facility)) {
+            facilities.remove(facility);
+        }
+    }
+
+    public void addFacilities(String facility) {
+        facilities.add(facility);
+    }
+
+}
