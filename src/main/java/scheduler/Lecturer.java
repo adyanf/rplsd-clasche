7a2fdb3ff695138f864bbc9efb3b6933c94ec58f
Reiva5 <jehiannormansaviero@gmail.com>
2018-11-22 01:51:40 +0700

    Add Class Lecturer

---
+++ src/main/java/scheduler/Lecturer.java
@@ -0,0 +1,31 @@
+package scheduler;
+
+import java.util.ArrayList;
+
+public class Lecturer {
+    private String name;
+    private ArrayList<ArrayList<Boolean>> availability;
+
+    public Lecturer(String name, ArrayList<ArrayList<Boolean>> availability) {
+        this.name = name;
+        this.availability = availability;
+    }
+
+    public ArrayList<ArrayList<Boolean>> getAvailability() {
+        return availability;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAvailability(Integer day, Integer hour, Boolean statusAvailability) {
+        if (day < availability.size() && hour < availability.get(0).size()) {
+            availability.get(day).set(hour, statusAvailability);
+        }
+    }
+}
